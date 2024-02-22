74d620b6a5a3b9d8cfd94470fd6669bc27ac200e
Kes <chelovekrukzak@gmail.com>
2024-02-22 20:12:05 +0600

    5lab

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-a = "            "
+import re
 
-print(len(a))
+with open("Lab5/row.txt", "r") as f:
+    for line in f:
+        line = re.sub(r'[ ,.]', ':', line)
+        print(line)
+                
+
+
+
